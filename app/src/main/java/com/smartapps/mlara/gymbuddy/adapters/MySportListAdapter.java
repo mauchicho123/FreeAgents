702f731114a149d636f4849747b1606b2e7b2799
mlara <mauriciorl505@hotmail.com>
2015-12-17 17:08:16 -0500

    Committing add sport code

---
+++ app/src/main/java/com/smartapps/mlara/gymbuddy/adapters/MySportListAdapter.java
@@ -0,0 +1,60 @@
+package com.smartapps.mlara.gymbuddy.adapters;
+
+import android.content.Context;
+import android.graphics.drawable.Drawable;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ImageButton;
+import android.widget.ImageView;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
+
+import com.smartapps.mlara.gymbuddy.R;
+import com.smartapps.mlara.gymbuddy.pojos.Sport;
+
+import java.util.List;
+
+/**
+ * Created by mlara on 12/9/2015.
+ */
+public class MySportListAdapter extends ArrayAdapter<Sport> {
+
+    private int resource;
+    private LayoutInflater inflater;
+    private Context context;
+
+    public MySportListAdapter(Context context, int resource, List<Sport> objects) {
+        super(context, resource, objects);
+        this.resource = resource;
+        inflater = LayoutInflater.from( context );
+        this.context=context;
+    }
+
+    @Override
+    public View getView ( int position, View convertView, ViewGroup parent ) {
+        Log.v("CHECK", " Inside MySportListAdapter getView");
+        convertView = (RelativeLayout) inflater.inflate( resource, null );
+        Sport sport= getItem( position );
+        TextView sportName = (TextView) convertView.findViewById(R.id.sportName);
+        sportName.setText(sport.getSportName());
+
+        TextView levelOfExpertise = (TextView) convertView.findViewById(R.id.levelOfExpertise);
+        levelOfExpertise.setText(sport.getLevelOfExpertise());
+
+        ImageView sportImage = (ImageView) convertView.findViewById(R.id.sportImage);
+        String uri = "drawable/" + sport.getSportName().toLowerCase();
+        int imageResource = context.getResources().getIdentifier(uri, null, context.getPackageName());
+        Drawable image = context.getResources().getDrawable(imageResource);
+        sportImage.setImageDrawable(image);
+
+        ImageButton trashCanButton = (ImageButton) convertView.findViewById(R.id.deleteSport);
+        uri = "drawable/trashcan";
+        imageResource = context.getResources().getIdentifier(uri, null, context.getPackageName());
+        image = context.getResources().getDrawable(imageResource);
+        trashCanButton.setImageDrawable(image);
+        return convertView;
+    }
+}
